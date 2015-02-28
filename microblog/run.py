a0378a8da2a7a0597b27e40b05ebca9b27b29518
grollcake <grollcake@gmail.com>
2015-02-28 02:00:03 -0500

    from devnory.dnip.net

---
+++ microblog/run.py
@@ -0,0 +1,5 @@
+#!flask/bin/python
+# _*_ coding: utf-8 _*_
+from app import app
+app.run(debug=True, host='0.0.0.0')
+
---
+++ python/study/study1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+# -*- coding: utf-8 -*-
+
+"""
+파이썬 학습용 스크립트 #1
+"""
+__author__ = "grollcake@gmail.com"
+
+
+def mydef(s):
+    print("안녕하세요," + s + "님\n")
+
+if __name__ == '__main__':
+    mydef('훈균')
+    mydef('Hardy!')
